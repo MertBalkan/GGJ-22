d1847b6dbfa05c896b6b4e6556267c94e38c3872
Mert Balkan <mert__balkan@hotmail.com>
2022-01-26 23:12:12 +0300

    sign controller added

---
+++ Assets/GameFolders/Scripts/Concretes/Controllers/SignController.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SignController : MonoBehaviour
+{
+    private string _writeText;
+    public string WriteText { get => _writeText; set => _writeText = value; }
+}
