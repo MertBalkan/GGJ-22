04d354c21d197dba0856e117e802868b288f0ef2
Mert Balkan <mert__balkan@hotmail.com>
2022-01-30 13:12:38 +0300

    new

--- Assets/GameFolders/Scripts/Concretes/Movements/CameraMove.cs
+++ Assets/GameFolders/Scripts/Concretes/Movements/CameraMove.cs
@@ -7,6 +7,13 @@ public class CameraMove : MonoBehaviour
     [SerializeField] private float _finalCameraMoveSpeed;
     private void Update()
     {
-        this.gameObject.transform.Translate(Vector3.right * Time.deltaTime * _finalCameraMoveSpeed);
+        if(this.gameObject.transform.position.x < 17.00f || this.gameObject.transform.position.x > 0.0f){
+            this.gameObject.transform.Translate(Vector3.right * Time.deltaTime * _finalCameraMoveSpeed);
+        }
+        else if(this.gameObject.transform.position.x > -1.00f || this.gameObject.transform.position.x < 0.0f){
+
+            this.gameObject.transform.Translate(Vector3.left * Time.deltaTime * _finalCameraMoveSpeed);
+
+        }
     }
 }
